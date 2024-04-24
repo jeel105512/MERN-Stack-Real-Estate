24976b44d9a2419e263ef743c3435dd909b61002
jeel105512 <jeelhpatel10@gmail.com>
2024-04-24 14:03:32 -0400

    Added connection to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => console.log(error));
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server us running on server http://localhost:3000");
+  console.log("Server us running on server http://localhost:3000");
 });
