1f74c99a28b341655455e261c9e8fed242b6c15b
jeel105512 <jeelhpatel10@gmail.com>
2024-04-24 13:52:25 -0400

    Clean up

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ import express from 'express';
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server us running on http://localhost:3000");
+    console.log("Server us running on server http://localhost:3000");
 });
