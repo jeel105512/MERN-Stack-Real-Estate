dda8d1908e7cb3263ec775a7b31aea07fe5b5e68
jeel105512 <jeelhpatel10@gmail.com>
2024-04-26 12:59:17 -0400

    Created the user auth sign-in route and controller action in the api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import { errorHandler } from "../utils/error.js";
 
@@ -14,6 +15,34 @@ export const signup = async (req, res, next) => {
     await newUser.save();
 
     res.status(201).json({ message: "SUCCESS", status: "201", newUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Credentials!"));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const {password: pass, ...userInfo} = user._doc; // to remove the password for the response
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        // expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(userInfo);
   } catch (error) {
     next(error);
   }
