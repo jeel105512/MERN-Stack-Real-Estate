84a1ddab1ba2ed93761027c73fc7db718c9a5a12
jeel105512 <jeelhpatel10@gmail.com>
2024-05-08 12:07:42 -0400

    Created search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -79,3 +79,55 @@ export const show = async (req, res, next) => {
     next(error);
   }
 };
+
+export const search = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9; // req.querry => http:/local.../search?limit=300 => "?limit=300"
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] }; // search in the database for both true and false
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const lisings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({
+        [sort]: order,
+      })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      lisings,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { show, create, update, remove } from "../controllers/listing.controller.js";
+import { show, create, update, remove, search } from "../controllers/listing.controller.js";
 import { varifyToken } from "../utils/varifyUser.js";
 
 const router = express.Router();
@@ -9,5 +9,8 @@ router.get("/show/:id", show);
 router.post("/create", varifyToken, create);
 router.post("/update/:id", varifyToken, update);
 router.delete("/delete/:id", varifyToken, remove);
+
+// serch routes
+router.get("/search", search);
 
 export default router;
