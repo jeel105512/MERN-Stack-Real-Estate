b7c3be34ad478fce3800dfffbbe1789a5fae8679
jeel105512 <jeelhpatel10@gmail.com>
2024-05-09 12:32:02 -0400

    Created the ListingCard component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import ListingCard from '../components/ListingCard/ListingCard';
 
 export default function Search() {
     const navigate = useNavigate();
@@ -65,7 +66,7 @@ export default function Search() {
             const sort = event.target.id.split("_")[0] || "created_at";
             const order = event.target.id.split("_")[1] || "desc";
             setSidebarData({ ...sidebarData, sort, order });
-        }        
+        }
     }
 
     const handleSubmit = (event) => {
@@ -132,8 +133,13 @@ export default function Search() {
                     <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Search</button>
                 </form>
             </div>
-            <div className="">
+            <div className="flex-1">
                 <h1 className='text-3xl font-semibold border-b p-3 mt-5 text-slate-700'>Listing results:</h1>
+                <div className="p-7 flex flex-wrap gap-4">
+                    {!loading && listings.length === 0 && (<p className='text-xl text-slate-700'>No listing found!</p>)}
+                    {loading && (<p className='text-xl text-slate-700 text-center w-full'>Loading...</p>)}
+                    {!loading && listings.listings && listings.listings.map((listing) => <ListingCard key={listing._id} listing={listing} />)}
+                </div>
             </div>
         </div>
     )
