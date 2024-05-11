150ce547efbefaba533b72497de985afe5ad147b
jeel105512 <jeelhpatel10@gmail.com>
2024-05-11 10:25:16 -0400

    Added pre installation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
@@ -14,6 +15,8 @@ mongoose
     console.log("Connected to MongoDB");
   })
   .catch((error) => console.log(error));
+
+  const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,6 +32,12 @@ app.listen(3000, () => {
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
