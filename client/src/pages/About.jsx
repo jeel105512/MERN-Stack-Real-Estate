fffacb3a39e55848bec413c7c003264a6e528f13
jeel105512 <jeelhpatel10@gmail.com>
2024-05-11 10:17:48 -0400

    Completed About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Jeel Estate</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum dolorem, magni, fuga molestiae aliquid consectetur numquam officiis explicabo ipsam at et eaque, ut eligendi quaerat tempore totam ab labore ad praesentium corporis quo doloremque illo quisquam? Sit aut temporibus ipsum.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae repellat nobis, error id quisquam pariatur molestias? Velit exercitationem non cupiditate est aperiam, dolorem repudiandae voluptas mollitia in nemo dicta, eius quod sint, quia vero soluta.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda rem quas culpa fuga fugiat nostrum, quod id consequatur architecto eum consectetur beatae a distinctio facilis quam dolorem rerum? Rerum vero, unde illo quasi repellendus quae recusandae eaque aliquid sequi dicta nostrum quod repellat labore dolore!</p>
+    </div>
   )
 }
